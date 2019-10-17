7196b5599672c8c5d0552404090a548d517a7d3c
qinyuyang1998 <49479089+qinyuyang1998@users.noreply.github.com>
2019-10-17 16:36:22 +0800

    Update MyApplication.java
    想加一行注释

--- src/main/java/com/sinosoft/MyApplication.java
+++ src/main/java/com/sinosoft/MyApplication.java
@@ -3,6 +3,7 @@ package com.sinosoft;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// 想加一行注释
 @SpringBootApplication
 public class MyApplication {
 	public static void main(String[] args) {
